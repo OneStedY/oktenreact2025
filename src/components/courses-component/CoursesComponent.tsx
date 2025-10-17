4b67e2653bfb83431043b585917e7b88c87348b8
OneStedY <trofimovartem321@gmail.com>
2025-10-17 19:20:13 +0300

    commit/HW1/#Fs05Kiho

---
+++ src/components/courses-component/CoursesComponent.tsx
@@ -0,0 +1,19 @@
+import "./CoursesComponent.css"
+import {coursesAndDurationArray} from "../../../arrays-2.ts";
+import {CourseComponent} from "../course-component/CourseComponent.tsx";
+import type {CourseModel} from "../../models/CourseModel.ts";
+
+export const CoursesComponent = () =>{
+
+
+
+    return(
+        <ul>
+            {
+                coursesAndDurationArray.map((course:CourseModel, index:number) =>{
+                    return <CourseComponent course={course} key={index} />;
+                })
+            }
+        </ul>
+    );
+};
