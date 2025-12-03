033eaebc4d33380020d9f23101d5333ff62f3400
OneStedY <trofimovartem321@gmail.com>
2025-12-03 16:42:49 +0200

    cf69cHp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-
+import {useFetch} from "./hooks/useFetch.tsx"
 import './App.css'
 
 
 function App() {
-
+  const users = useFetch<{ id:number, name:string }[]>('https://jsonplaceholder.typicode.com/users', [])
 
   return (
     <>
-
+      {
+        users.map((user)=>(
+            <div key={user.id}>
+              {user.id} {user.name}
+            </div>
+        ))
+      }
     </>
   );
 }
